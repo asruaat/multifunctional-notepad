860b79005d26dbf55624c968d119d5e7f1619055
asruaat <dsdasa1993@gmail.com>
2015-11-18 21:23:53 +0100

    maybe final
    maybe final

--- delete.js
+++ delete.js
@@ -1,22 +1,24 @@
-             window.navigator.vibrate(2000);
+window.onload = function() {  
+	window.navigator.vibrate(2000);
+}
 			 
-			function vissza() {
+function vissza() {
                 window.location.href = "index.html";
-            }
+}
             
-            function bezar() {
-                var IDBOpenDBRequest_req1 = window.indexedDB.deleteDatabase ("Adatbazis_Tar");
-                IDBOpenDBRequest_req1.onsuccess = function () {
+function bezar() {
+    var IDBOpenDBRequest_req1 = window.indexedDB.deleteDatabase ("Adatbazis_Tar");
+        IDBOpenDBRequest_req1.onsuccess = function () {
 				 
-                  console.log ("Sikerült az adatbázis törlése!");    
-                };
+			console.log ("Sikerült az adatbázis törlése!");    
+    };
               
-                IDBOpenDBRequest_req1.onerror = function () {
-                  console.log ("Hiba az adatbázis törlésekor!");    
-                };
+		IDBOpenDBRequest_req1.onerror = function () {
+			console.log ("Hiba az adatbázis törlésekor!");    
+        };
               
                 window.location.href = "index.html";
-            }
+        }
 
-            document.querySelector("#nem").addEventListener("click", vissza);
-            document.querySelector("#igen").addEventListener("click", bezar);
+document.querySelector("#nem").addEventListener("click", vissza);
+document.querySelector("#igen").addEventListener("click", bezar);
